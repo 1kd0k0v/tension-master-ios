bdd6164693420669fbbf4a3399c48cfb9934bd5e
Nikolay Markov <nmarkov1991@gmail.com>
2019-02-08 01:48:21 +0200

    Update version to 1.0.4(4) + Fix archiving issue

--- TensionMaster/Utility/Colors.swift
+++ TensionMaster/Utility/Colors.swift
@@ -24,21 +24,6 @@ extension UIColor {
                   a: a)
     }
     
-    convenience init(red: Int, green: Int, blue: Int, a: Int = 0xFF) {
-        self.init(red: CGFloat(red) / 255.0,
-                  green: CGFloat(green) / 255.0,
-                  blue: CGFloat(blue) / 255.0,
-                  alpha: CGFloat(a) / 255.0)
-    }
-    
-    // let's suppose alpha is the first component (ARGB)
-    convenience init(argb: Int) {
-        self.init(red: (argb >> 16) & 0xFF,
-                  green: (argb >> 8) & 0xFF,
-                  blue: argb & 0xFF,
-                  a: (argb >> 24) & 0xFF)
-    }
-    
 }
 
 extension UIColor {
@@ -72,7 +57,7 @@ extension UIColor {
     }
     
     class var secondaryText: UIColor {
-        return UIColor(argb: 0xBBFFFFFF)
+        return UIColor(rgb: 0xFFFFFF, a: CGFloat(0xBB) / 0xFF)
     }
 
 }
