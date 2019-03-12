39135e259f1d351a4a7b58ebfc6ab9d97d2d4e3c
Nikolay Markov <nmarkov1991@gmail.com>
2019-03-13 00:11:19 +0200

    Remove lagacy hybrid string types

--- TensionMaster/Utility/Settings.swift
+++ TensionMaster/Utility/Settings.swift
@@ -14,28 +14,22 @@ enum MeasureMode: String {
 }
 
 enum StringType: String {
-    case polyester = "Polyester, Co-polyester"
+    case polyester = "Polyester"
+    case copolyester = "Co-polyester"
     case syntheticGut = "Synthetic Gut"
     case naturalGut = "Natural Gut"
-    case natGutAndPoly = "Hybrid - natural gut/polyester"
-    case synthGutAndPoly = "Hybrid - synthetic gut/polyester"
-    case natGutAndSynthGut = "Hybrid - natural/synthetic gut"
     static var allRepresentations: [String] {
         return [polyester.rawValue,
+                copolyester.rawValue,
                 syntheticGut.rawValue,
-                naturalGut.rawValue,
-                natGutAndPoly.rawValue,
-                synthGutAndPoly.rawValue,
-                natGutAndSynthGut.rawValue]
+                naturalGut.rawValue]
     }
     var coefficient: Double {
         switch self {
         case .polyester: return 1.35
+        case .copolyester: return 1.35
         case .syntheticGut: return 1.12
         case .naturalGut: return 1.28
-        case .natGutAndPoly: return 1.3
-        case .synthGutAndPoly: return 1.24
-        case .natGutAndSynthGut: return 1.21
         }
     }
 }
