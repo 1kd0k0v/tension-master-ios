85c351e51a92097dd2ba393bffc7f8f1c51ffba8
Nikolay Markov <nmarkov1991@gmail.com>
2019-04-21 19:42:27 +0300

    Revert "Link audio kit manually .."
    This reverts commit 151fa885ed6812e6245f2ccad1f5523c77d56ce1.

--- TensionMaster/AppDelegate.swift
+++ TensionMaster/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         brand()
-        StorageManager.shared.load()
         
         return true
     }
@@ -43,8 +41,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
-        StorageManager.shared.saveContext()
     }
     
     private func brand() {
@@ -62,6 +58,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UITableView.appearance().backgroundColor = UIColor.backgroundDark
         UITableView.appearance().separatorColor = UIColor.secondaryText
     }
+
 
 }
 
